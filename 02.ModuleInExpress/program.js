a49d7e21f3a0ae6876e97608f648945738c35cac
prince kumar <agarwal8789@gmail.com>
2024-02-15 17:04:46 +0530

    commit made

---
+++ 02.ModuleInExpress/program.js
@@ -0,0 +1,4 @@
+const math=require('./math.js');
+console.log(math.add(2,3));
+console.log(math.sub(2,3));
+console.log(math.add(2,3)+math.sub(2,3));
---
+++ 03.FileModule/file.js
@@ -0,0 +1,19 @@
+const fs=require("fs");
+
+// fs.writeFile("example.txt","this is my first txt",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("File successfully created");
+//         fs.readFile("example.txt","utf8",(err,file)=>{
+//             if(err){
+//                 console.log(err);
+//             }
+//             else{
+//                 console.log(file);
+//             }
+//         })
+//     }
+// })
+fs.writeFileSync("./test.txt","hello world");
