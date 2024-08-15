3733e4d27114f984e38604fb6099dc685d0f5a98
prince kumar <agarwal8789@gmail.com>
2024-08-15 20:11:12 +0530

    commit made on week2 complete

--- 18.assignments/week-2/01-async-js/hard (promises)/3-promise-all.js
+++ 18.assignments/week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,20 +4,37 @@
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
-function wait1(t) {
-
+function wait1(t1) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, t1 * 1000); // Convert seconds to milliseconds
+    });
 }
 
-function wait2(t) {
-
+function wait2(t2) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, t2 * 1000); // Convert seconds to milliseconds
+    });
 }
 
-function wait3(t) {
-
+function wait3(t3) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, t3 * 1000); // Convert seconds to milliseconds
+    });
 }
 
 function calculateTime(t1, t2, t3) {
+    const startTime = Date.now(); // Record start time
 
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(() => {
+        const endTime = Date.now(); // Record end time
+        return endTime - startTime; // Calculate and return the time taken
+    });
 }
 
 module.exports = calculateTime;
--- 18.assignments/week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ 18.assignments/week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -4,21 +4,35 @@
  * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
-
-function wait1(t) {
-
+function wait(t) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, t * 1000); // Convert seconds to milliseconds
+    });
+}
+function wait1(t1) {
+    return wait(t1);
 }
 
-function wait2(t) {
-
+function wait2(t2) {
+    return wait(t2);
 }
 
-function wait3(t) {
-
+function wait3(t3) {
+    return wait(t3);
 }
 
 function calculateTime(t1, t2, t3) {
+    const startTime = Date.now(); // Record start time
 
+    return wait1(t1)
+        .then(() => wait2(t2))
+        .then(() => wait3(t3))
+        .then(() => {
+            const endTime = Date.now(); // Record end time
+            return endTime - startTime; // Calculate and return the time taken
+        });
 }
 
 module.exports = calculateTime;
