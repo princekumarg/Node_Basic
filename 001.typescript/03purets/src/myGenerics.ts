0b3384b3271ca6b612c318e8430a6ccaa0d86690
prince kumar <agarwal8789@gmail.com>
2024-08-29 09:25:59 +0530

    commit made on update

--- 001.typescript/03purets/src/myGenerics.ts
+++ 001.typescript/03purets/src/myGenerics.ts
@@ -1,74 +1,57 @@
-const score: Array<number> = []
-const names:Array<string> = []
-
-function identityOne(val: boolean | number): boolean | number{
-    return val
+const score: Array<number> = [];
+const names: Array<string> = [];
+function identityOne(val: boolean | number): boolean | number {
+  return val;
 }
-
-function identityTwo(val: any):any{
-    return val
+function identityTwo(val: any): any {
+  return val;
 }
-
 function identityThree<Type>(val: Type): Type {
-    return val
+  return val;
 }
-
 // identityThree(true)
-
 function identityFour<T>(val: T): T {
-    return val
+  return val;
 }
-
-interface Bootle{
-    brand: string,
-    type: number
+interface Bootle {
+  brand: string;
+  type: number;
 }
-
 // identityFour<Bootle>({})
-
 function getSearchProducts<T>(products: T[]): T {
-    // do some database operations
-    const myIndex = 3
-    return products[myIndex]
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
 }
-
-
-const getMoreSearchProducts = <T,>(products: T[]): T => {
-    //do some database operations
-    const myIndex = 4
-    return products[myIndex]
-}
-
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  //do some database operations
+  const myIndex = 4;
+  return products[myIndex];
+};
 interface Database {
-    connection: string,
-    username: string,
-    password: string
+  connection: string;
+  username: string;
+  password: string;
 }
-
-function anotherFunction<T, U extends Database>(valOne:T, valTwo:U):object {
-    return{
-        valOne,
-        valTwo
-    }
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
 }
-
 // anotherFunction(3, {})
-
-interface Quiz{
-    name: string,
-    type: string
+interface Quiz {
+  name: string;
+  type: string;
 }
-
-interface Course{
-    name: string,
-    author: string,
-    subject: string
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
 }
-
-class Sellable<T>{
-    public cart: T[] = []
-
-    addToCart(product: T){
-        this.cart.push(product)
-    }
+class Sellable<T> {
+  public cart: T[] = [];
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
 }
