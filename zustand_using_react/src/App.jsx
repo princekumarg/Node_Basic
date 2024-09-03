0cd9c6cefb7923e0260919feb8931712a5b51f20
prince kumar <agarwal8789@gmail.com>
2024-09-03 23:10:31 +0530

    commit made on zustand

---
+++ zustand_using_react/src/App.jsx
@@ -0,0 +1,15 @@
+import Columns from "./columns"
+
+function App() {
+  return (
+    <>
+      <section className='flex h-screen bg-gradient-to-br from-gray-700 to-gray-900 py-12 text-white'>
+      <div className='mx-auto w-full max-w-7xl px-6'>
+        <Columns/>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default App
---
+++ zustand_using_react/src/lib/store.js
@@ -0,0 +1,31 @@
+import { create } from "zustand"
+import { v4 as uuid } from "uuid"
+import { persist } from "zustand/middleware"
+
+export const useTaskStore = create()(
+    persist(
+        set => ({
+            tasks: [],
+            draggedTask: null,
+            addTask: (title, description) =>
+                set(state => ({
+                    tasks: [
+                        ...state.tasks,
+                        { id: uuid(), title, description, status: "TODO" }
+                    ]
+                })),
+            dragTask: id => set({ draggedTask: id }),
+            removeTask: id =>
+                set(state => ({
+                    tasks: state.tasks.filter(task => task.id !== id)
+                })),
+            updateTask: (id, status) =>
+                set(state => ({
+                    tasks: state.tasks.map(task =>
+                        task.id === id ? { ...task, status } : task
+                    )
+                }))
+        }),
+        { name: "task-store", skipHydration: true }
+    )
+)
